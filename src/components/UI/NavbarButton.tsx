bbe8dbd316b5d39388d4603c144928ad979c927e
Yury Borets <udborets@gmail.com>
2023-03-16 00:21:39 +0300

    add navbar button props interface

--- src/components/UI/NavbarButton.tsx
+++ src/components/UI/NavbarButton.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import Image, { StaticImageData } from "next/image"
 
-const NavbarButton = ({ text, navPath, img }: { text: string; navPath: string, img: StaticImageData }) => {
+interface NavbarButtonProps {
+  text: string;
+  navPath: string;
+  img: StaticImageData;
+  className?: string;
+}
+
+const NavbarButton = ({ text, navPath, img, className }: NavbarButtonProps) => {
   return (
     <div className="p-2 bg-opacity-0 w-full">
       <Link
         href={navPath}
-        className="text-white w-full text-[1rem] items-center hover:tracking-[0.08rem] transition-all duration-[0.5s] ease-out flex gap-[0.5rem]"
+        className={"text-white w-full text-[1rem] items-center hover:tracking-[0.08rem] transition-all duration-[0.5s] ease-out flex gap-[0.5rem] " + className ?? ""}
       >
         <Image
           src={img}
