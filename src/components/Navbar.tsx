0cc231af7fe1cf3bca26ccfa89458a42eb88781f
Yury Borets <udborets@gmail.com>
2023-03-16 00:21:21 +0300

    add navbar props interface

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
-const Navbar = ({ children, className }: { children: JSX.Element | JSX.Element[], className?: string }) => {
+interface NavbarProps {
+  children: JSX.Element | JSX.Element[];
+  className?: string;
+}
+
+const Navbar = ({ children, className }: NavbarProps) => {
   return (
     <aside className="nav flex w-[150px] flex-col justify-center bg-black px-2">
-      <nav className={`nav__menu flex flex-col aling-center h-fit justify-around gap-5 ${className ?? ""}`}>
+      <nav className={"nav__menu flex flex-col aling-center h-fit justify-around gap-5" + className ?? ""}>
         {children}
       </nav>
     </aside>
