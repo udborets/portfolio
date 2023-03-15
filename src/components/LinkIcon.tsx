f02ec1d4969b746ba371bbb5cf7c97da4d5f23de
Yury Borets <udborets@gmail.com>
2023-03-16 01:35:39 +0300

    delete link icon div parent

--- src/components/LinkIcon.tsx
+++ src/components/LinkIcon.tsx
@@ -8,11 +8,9 @@ interface LinkIconProps {
 
 const LinkIcon = ({ link, icon, alt }: LinkIconProps) => {
   return (
-    <div>
       <a href={link} className="w-fit h-fit">
         <Image alt={alt} title={alt} src={icon} className="w-[60px] h-[60px] dev-icon-effect" />
       </a>
-    </div>
   )
 }
 
