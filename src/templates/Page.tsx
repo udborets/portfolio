fe22374757f0d14e4c89b65d6b25ad7867bdc28d
Yury Borets <udborets@gmail.com>
2023-03-15 09:38:49 +0300

    add navbar to page template

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -1,12 +1,22 @@
+import Navbar from "@/components/Navbar"
+import NavbarButton from "@/components/UI/NavbarButton"
+
 export const Page = ({ className, children }:
   { className?: string, children: JSX.Element }) => {
   return (
-    <div className="page">
-      <div className='page__container'>
-        <div className={`flex w-full h-full ${className ?? ""}`} >
-          {children}
+    <>
+      <Navbar>
+        <NavbarButton text='Home' navPath='/' />
+        <NavbarButton text="About" navPath="about" />
+        <NavbarButton text="Projects" navPath="projects" />
+      </Navbar>
+      <div className="page">
+        <div className='page__container'>
+          <div className={`flex w-full h-full ${className ?? ""}`} >
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
