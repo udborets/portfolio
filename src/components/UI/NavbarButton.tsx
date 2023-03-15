11d638bcf47ada9e1d66388c29b8004ef0e0a311
Yury Borets <udborets@gmail.com>
2023-03-15 19:20:44 +0300

    add img to navbar button, fix styles

--- src/components/UI/NavbarButton.tsx
+++ src/components/UI/NavbarButton.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import Image, { StaticImageData } from "next/image"
 
-const NavbarButton = ({ text, navPath }: { text: string; navPath: string }) => {
+const NavbarButton = ({ text, navPath, img }: { text: string; navPath: string, img: StaticImageData }) => {
   return (
-    <div className="bg-white p-2 bg-opacity-0 flex w-full">
-      <Link href={navPath} className="text-white w-full text-[1rem] hover:tracking-[0.08rem] transition-all duration-[0.5s] ease-out">{text}</Link>
+    <div className="p-2 bg-opacity-0 w-full">
+      <Link
+        href={navPath}
+        className="text-white w-full text-[1rem] hover:tracking-[0.08rem] transition-all duration-[0.5s] ease-out flex gap-[0.5rem]"
+      >
+        <Image
+          src={img}
+          className="w-[15px] h-[15px] mt-[4px] flex"
+          alt="*"
+        />
+        {text}
+      </Link>
     </div>
   )
 }
