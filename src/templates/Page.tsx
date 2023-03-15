7efa7b8c2a756853c8f5f034285deee96455c0da
Yury Borets <udborets@gmail.com>
2023-03-15 19:33:18 +0300

    add images for navbar buttons

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -1,14 +1,17 @@
 import Navbar from "@/components/Navbar"
 import NavbarButton from "@/components/UI/NavbarButton"
+import homeIcon from '../assets/homeIcon.png'
+import aboutIcon from '../assets/aboutIcon.png'
+import projectsIcon from '../assets/projectsIcon.png'
 
 export const Page = ({ className, children }:
   { className?: string, children: JSX.Element | JSX.Element[] }) => {
   return (
     <>
       <Navbar>
-        <NavbarButton text='Home' navPath='/' />
-        <NavbarButton text="About" navPath="about" />
-        <NavbarButton text="Projects" navPath="projects" />
+        <NavbarButton text='Home' navPath='/' img={homeIcon} />
+        <NavbarButton text="About" navPath="about" img={aboutIcon} />
+        <NavbarButton text="Projects" navPath="projects" img={projectsIcon} />
       </Navbar>
       <main className="page flex flex-col w-full h-full p-5 bg-[#111111]">
         <div className='page-container flex h-full flex-col flex-grow'>
