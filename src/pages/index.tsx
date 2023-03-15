28833c2c7c0fdb44bb1ff7c2f1e0cda16389ff3c
Yury Borets <udborets@gmail.com>
2023-03-15 17:32:34 +0300

    fix classes, add tech usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,26 @@
+import Tech from "@/components/Tech"
 import { Page } from "@/templates/Page"
 
 const Home = () => {
   return (
     <Page className="items-center justify-center">
       <div className="flex flex-col gap-[1.5rem]">
-        <span className="text-[3rem]">
+        <span className="text-[3rem] w-fit">
           Hi, my name is Yura,
         </span>
-        <span className="text-[2rem]">
+        <span className="text-[2rem] w-fit">
           and I am a frontend developer!
         </span>
-        <div className="flex gap-[0.5rem]">
-          <span className="flex flex-row align-center justify-center text-[1.5rem]">
-            In my projects I use
-            &nbsp; <i className="devicon-typescript-plain colored mt-[9px]" /> &nbsp; TypeScript,
-            &nbsp; <i className="devicon-react-original colored mt-[9px]" /> &nbsp; React,
-            &nbsp; <i className="devicon-express-original colored mt-[9px]" /> &nbsp; Express,
-            &nbsp; <i className="devicon-nextjs-original colored mt-[9px]" /> &nbsp; Next,
-            &nbsp; <i className="devicon-tailwindcss-plain colored mt-[9px]" /> &nbsp; Tailwind,
-            &nbsp; <i className="devicon-redux-original colored mt-[9px]" /> &nbsp; Redux
-          </span>
+        <div className="flex gap-[0.5rem] flex-col flex-wrap">
+          <div className="flex flex-row align-center justify-start text-[1.5rem] flex-wrap">
+            In my projects I use:
+            <Tech iconClass="devicon-typescript-plain colored" text="TypeScript," />
+            <Tech iconClass="devicon-react-original colored" text="React," />
+            <Tech iconClass="devicon-nextjs-original colored" text="Next," />
+            <Tech iconClass="devicon-tailwindcss-plain colored" text="Tailwind," />
+            <Tech iconClass="devicon-express-original colored colored" text="Express," />
+            <Tech iconClass="devicon-redux-original colored" text="Redux" />
+          </div>
         </div>
       </div>
     </Page>
