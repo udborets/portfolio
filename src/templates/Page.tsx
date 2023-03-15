8fe21035e3cc9283ad1ea97b960880a492b9a89e
Yury Borets <udborets@gmail.com>
2023-03-15 17:33:05 +0300

    change page html tag

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -10,13 +10,13 @@ export const Page = ({ className, children }:
         <NavbarButton text="About" navPath="about" />
         <NavbarButton text="Projects" navPath="projects" />
       </Navbar>
-      <div className="page flex flex-col w-full h-full p-5">
+      <main className="page flex flex-col w-full h-full p-5">
         <div className='page-container flex h-full flex-col flex-grow'>
           <div className={`content-wrapper flex flex-grow ${className ?? ""}`} >
             {children}
           </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
