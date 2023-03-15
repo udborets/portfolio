9e982e623344979c9d2f3e646095d1afbd3de5b1
Yury Borets <udborets@gmail.com>
2023-03-15 11:35:02 +0300

    fix tech stack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Page } from "@/templates/Page"
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next"
 
 const Home = () => {
   return (
@@ -13,10 +14,12 @@ const Home = () => {
         <div className="flex gap-[0.5rem]">
           <span className="flex flex-row align-center justify-center text-[1.5rem]">
             In my projects I use
+            &nbsp; <i className="devicon-typescript-plain colored mt-[9px]" /> &nbsp; TypeScript,
             &nbsp; <i className="devicon-react-original colored mt-[9px]" /> &nbsp; React,
-            &nbsp; <i className="devicon-express-original colored mt-[9px]" /> &nbsp; Next,
+            &nbsp; <i className="devicon-express-original colored mt-[9px]" /> &nbsp; Express,
+            &nbsp; <i className="devicon-nextjs-original colored mt-[9px]" /> &nbsp; Next,
             &nbsp; <i className="devicon-tailwindcss-plain colored mt-[9px]" /> &nbsp; Tailwind,
-            &nbsp; <i className="devicon-redux-original colored mt-[9px]" /> &nbsp; Redux,
+            &nbsp; <i className="devicon-redux-original colored mt-[9px]" /> &nbsp; Redux
           </span>
         </div>
       </div>
