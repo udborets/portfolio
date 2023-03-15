84db4556d0a288c12043dcce51b627ca563e6479
Yury Borets <udborets@gmail.com>
2023-03-15 18:28:03 +0300

    fix navbar button styles

--- src/components/UI/NavbarButton.tsx
+++ src/components/UI/NavbarButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NavbarButton = ({ text, navPath }: { text: string; navPath: string }) => {
   return (
     <div className="bg-white p-2 bg-opacity-0 flex w-full">
-      <Link href={navPath} className="text-white w-full text-[2rem] hover:tracking-[0.1rem] transition-all duration-[0.5s] ease-out">{text}</Link>
+      <Link href={navPath} className="text-white w-full text-[1rem] hover:tracking-[0.08rem] transition-all duration-[0.5s] ease-out">{text}</Link>
     </div>
   )
 }
