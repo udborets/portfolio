3ab35e85e8b212137849052d4685a3b88616611b
Yury Borets <udborets@gmail.com>
2023-03-16 00:21:30 +0300

    add tech icon props interface

--- src/components/TechIcon.tsx
+++ src/components/TechIcon.tsx
@@ -1,4 +1,9 @@
-const TechIcon = ({ text, iconClass }: { text: string; iconClass: string }) => {
+interface TechIconProps {
+  text: string;
+  iconClass: string;
+}
+
+const TechIcon = ({ text, iconClass }: TechIconProps) => {
   return (
     <span className="flex justify-center items-center text-white"><i className={`${iconClass} mt-[4px] mx-[1rem]`} />{text}</span>
   )
