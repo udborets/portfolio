e626dfb455bbd28e94500e5ee881f9ba0aa8e5c0
Yury Borets <udborets@gmail.com>
2023-03-15 11:07:56 +0300

    fix navbar class

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = ({ children, className }: { children: JSX.Element | JSX.Element[], className?: string }) => {
   return (
-    <div className={`flex gap-[10%] aling-center justify-center ${className ?? ""}`}>
+    <div className={`flex gap-[10%] aling-center h-fit justify-center ${className ?? ""}`}>
       {children}
     </div>
   )
