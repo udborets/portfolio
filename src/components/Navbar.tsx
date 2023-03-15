9d846b2bce5ef6f99cebdce3ad3d9c1e3fd3752e
Yury Borets <udborets@gmail.com>
2023-03-15 22:26:42 +0300

    fix navbar buttons positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = ({ children, className }: { children: JSX.Element | JSX.Element[], className?: string }) => {
   return (
-    <aside className="nav flex w-[150px] flex-col items-center justify-center bg-black px-2">
+    <aside className="nav flex w-[150px] flex-col justify-center bg-black px-2">
       <nav className={`nav__menu flex flex-col aling-center h-fit justify-around gap-5 ${className ?? ""}`}>
         {children}
       </nav>
