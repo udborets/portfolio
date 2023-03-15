d7204ff6ee381b536e5725116728352f608964ce
Yury Borets <udborets@gmail.com>
2023-03-15 19:20:21 +0300

    fix navbar padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = ({ children, className }: { children: JSX.Element | JSX.Element[], className?: string }) => {
   return (
-    <aside className="nav flex w-[150px] flex-col align-center justify-center bg-black px-5">
+    <aside className="nav flex w-[150px] flex-col align-center justify-center bg-black px-2">
       <nav className={`nav__menu flex flex-col aling-center h-fit justify-around gap-5 ${className ?? ""}`}>
         {children}
       </nav>
