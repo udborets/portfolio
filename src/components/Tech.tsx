e895914ab06d6d82e2fdf49d998b1fb9053bf22c
Yury Borets <udborets@gmail.com>
2023-03-15 23:29:37 +0300

    delete tech component (rename)

--- src/components/Tech.tsx
+++
@@ -1,7 +0,0 @@
-const Tech = ({ text, iconClass }: { text: string; iconClass: string }) => {
-  return (
-    <span className="flex justify-center items-center text-white"><i className={`${iconClass} mt-[4px] mx-[1rem]`} />{text}</span>
-  )
-}
-
-export default Tech
