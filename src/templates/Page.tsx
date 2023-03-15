de8fc5a851a8cc43e53d257878c1e05a5e63a428
Yury Borets <udborets@gmail.com>
2023-03-15 09:36:03 +0300

    fix page component styles

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -1,9 +1,9 @@
-export const Page = ({ wrapperClass, children }:
-  { wrapperClass: string, children: JSX.Element }) => {
+export const Page = ({ className, children }:
+  { className?: string, children: JSX.Element }) => {
   return (
     <div className="page">
       <div className='page__container'>
-        <div className={wrapperClass}>
+        <div className={`flex w-full h-full ${className ?? ""}`} >
           {children}
         </div>
       </div>
