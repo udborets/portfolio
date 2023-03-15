8aaf015776af209308def75af20793ff3773a585
Yury Borets <udborets@gmail.com>
2023-03-15 17:58:57 +0300

    fix tech component styles

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,6 +1,6 @@
 const Tech = ({ text, iconClass }: { text: string; iconClass: string }) => {
   return (
-    <span className="flex justify-center align-center"><i className={`${iconClass} mt-[9px] mx-[1rem]`} />{text}</span>
+    <span className="flex justify-center align-center text-white"><i className={`${iconClass} mt-[9px] mx-[1rem]`} />{text}</span>
   )
 }
 
