15fbdd929574bf90b4401042807c79be10d5f15e
Yury Borets <udborets@gmail.com>
2023-03-16 16:41:11 +0300

    add "about" page paragraph component

---
+++ src/components/AboutParagraph.tsx
@@ -0,0 +1,21 @@
+import { motion as m } from "framer-motion";
+
+interface AboutParagraphProps {
+  children: string;
+  transitionDuration: number;
+  transitionDelay: number;
+}
+
+const AboutParagraph = ({ children: text, transitionDuration, transitionDelay }: AboutParagraphProps) => {
+  return (
+    <m.p
+      className="text-[1.5rem] text-white w-full text-center"
+      initial={{ translateY: "-20%", opacity: 0 }}
+      animate={{ translateY: "0%", opacity: 1 }}
+      transition={{ duration: transitionDuration, ease: "easeOut", delay: transitionDelay }}>
+      {text}
+    </m.p >
+  )
+}
+
+export default AboutParagraph
