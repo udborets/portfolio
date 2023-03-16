4b33841314a4c5245dfdb473a3d1bfa0ed1e6046
Yury Borets <udborets@gmail.com>
2023-03-16 10:59:12 +0300

    fix navbar scaling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ interface NavbarProps {
 
 const Navbar = ({ children, className }: NavbarProps) => {
   return (
-    <aside className="nav flex w-[150px] flex-col justify-center bg-black px-2">
+    <aside className="nav flex w-[150px] h-full min-w-[150px] flex-col justify-center bg-black px-2">
       <nav className={"nav__menu flex flex-col aling-center h-fit justify-around gap-5" + className ?? ""}>
         {children}
       </nav>
