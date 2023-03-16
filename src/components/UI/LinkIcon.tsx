406efc670a68faa7a7a4980b372582a8aa606350
Yury Borets <udborets@gmail.com>
2023-03-16 10:23:58 +0300

    change link icon component scale

--- src/components/UI/LinkIcon.tsx
+++ src/components/UI/LinkIcon.tsx
@@ -9,7 +9,7 @@ interface LinkIconProps {
 const LinkIcon = ({ link, icon, alt }: LinkIconProps) => {
   return (
       <a href={link} className="w-fit h-fit">
-        <Image alt={alt} title={alt} src={icon} className="w-[60px] h-[60px] dev-icon-effect" />
+        <Image alt={alt} title={alt} src={icon} className="w-[40px] h-[40px] dev-icon-effect" />
       </a>
   )
 }
