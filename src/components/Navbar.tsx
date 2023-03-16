4ba81185b5f6ab19b98416f2309c9dd07cb5e0cb
Yury Borets <udborets@gmail.com>
2023-03-16 22:16:02 +0300

    fix navbar responsibility styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ interface NavbarProps {
 
 const Navbar = ({ children, className }: NavbarProps) => {
   return (
-    <aside className="nav flex w-full h-fit flex-row justify-center bg-black px-2 z-[2] xl:flex-col xl:w-[150px] xl:h-full xl:min-w-[150px]">
+    <aside className="nav flex w-full h-[100px] min-h-[100px] items-center xl:items-start flex-row justify-center bg-black px-2 z-[2] xl:flex-col xl:w-[150px] xl:h-full xl:min-w-[150px]">
       <nav>
-        <ul className={"nav__menu flex flex-row xl:flex-col aling-center h-fit justify-around gap-5" + className ?? ""}>
+        <ul className={"nav__menu flex flex-row xl:flex-col aling-center h-fit justify-around gap-8 " + className ?? ""}>
           {children}
         </ul>
       </nav>
