d198c5fabdc630f2a3f194c37804fcc3bc665a2a
Yury Borets <udborets@gmail.com>
2023-03-16 18:58:43 +0300

    add movingFromTop page component attr

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -3,15 +3,16 @@ import { motion as m } from "framer-motion";
 interface PageProps {
   className?: string;
   children: JSX.Element | JSX.Element[];
+  movingFromTop?: boolean;
 }
-export const Page = ({ className, children }: PageProps) => {
-  const translateOrigin = Math.random() > 0.5;
-  if (translateOrigin) {
+
+export const Page = ({ className, children, movingFromTop }: PageProps) => {
+  if (movingFromTop) {
     return (
       <m.main
         className="page flex flex-col w-full h-full p-5 main-bg z-[1]"
-        initial={{ opacity: 0, translateX: "-20%" }}
-        animate={{ opacity: 1, translateX: "0%" }}
+        initial={{ opacity: 0, translateY: "-20%" }}
+        animate={{ opacity: 1, translateY: "0%" }}
         transition={{ duration: 1, ease: "easeOut" }}
       >
         <div className='page-container flex h-full flex-col flex-grow'>
@@ -25,8 +26,8 @@ export const Page = ({ className, children }: PageProps) => {
   return (
     <m.main
       className="page flex flex-col w-full h-full p-5 main-bg z-[1]"
-      initial={{ opacity: 0, translateY: "-20%" }}
-      animate={{ opacity: 1, translateY: "0%" }}
+      initial={{ opacity: 0, translateX: "-20%" }}
+      animate={{ opacity: 1, translateX: "0%" }}
       transition={{ duration: 1, ease: "easeOut" }}
     >
       <div className='page-container flex h-full flex-col flex-grow'>
