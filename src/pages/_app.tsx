dbdbd3440c75edc5d6473445eaa8e370eb1b6bfa
Yury Borets <udborets@gmail.com>
2023-03-17 16:56:33 +0300

    wrap app with redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,17 @@ import homeIconActive from '@/assets/homeIconActive.png';
 import Navbar from "@/components/Navbar";
 import NavbarButton from "@/components/UI/NavbarButton";
 import "@/styles/global.scss";
+import { Provider } from 'react-redux';
+import { store } from '@/store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Navbar>
         <NavbarButton text='Home' navPath='/' img={homeIcon} imgActive={homeIconActive} />
         <NavbarButton text="About" navPath="/about" img={aboutIcon} imgActive={aboutIconActive} />
       </Navbar>
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
