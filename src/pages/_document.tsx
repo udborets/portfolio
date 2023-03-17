cef688ef0fc6429b8321cfe4f3e2b254866ead17
Yury Borets <udborets@gmail.com>
2023-03-17 08:31:16 +0300

    delete user-scalable=no meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="https://img.icons8.com/external-others-inmotus-design/67/null/external-U-qwerty-keypad-others-inmotus-design-3.png" />
-        <meta name="viewport" content="user-scalable = no" />
       </Head>
       <body>
         <Main />
