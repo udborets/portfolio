69fe0f1dce23a3aecf6add7e7ad9a09f69cd7896
Yury Borets <udborets@gmail.com>
2023-03-18 12:03:01 +0300

    add test repos fetching in "projects" page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,15 +1,32 @@
+import axios from "axios";
 import Head from "next/head";
+import { useQuery } from "react-query";
 
 import { Page } from "@/templates/Page";
 
+type Repo = {
+  name: string;
+  html_url: string;
+  id: number;
+  language: string;
+}
+
 const Projects = () => {
+  const { data: repos } = useQuery({
+    queryFn: async () => {
+      const response = await axios.get("https://api.github.com/users/udborets/repos");
+      const repos = response.data as Repo[];
+      return repos;
+    }
+  })
+
   return (
     <>
       <Head>
         <title>/projects</title>
       </Head>
       <Page>
-        <div>projects page</div>
+        <div className="text-white">{repos ? repos.map((repo) => <a href={repo.html_url} key={repo.name}>{repo.name}</a>) : ' '}</div>
       </Page>
     </>
   )
