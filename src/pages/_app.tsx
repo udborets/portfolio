74cc07e1fdb1c6f207b62a697b7cff97bd835cac
Yury Borets <udborets@gmail.com>
2023-03-19 14:17:25 +0300

    add projects navbar button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
-import { QueryClient } from 'react-query';
-import { QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 
 import aboutIcon from '@/assets/aboutIcon.png';
 import aboutIconActive from '@/assets/aboutIconActive.png';
 import homeIcon from '@/assets/homeIcon.png';
 import homeIconActive from '@/assets/homeIconActive.png';
+import projectsIcon from '@/assets/projectsIcon.png';
+import projectsIconActive from '@/assets/projectsIconActive.png';
 import Navbar from "@/components/Navbar";
 import NavbarButton from "@/components/UI/NavbarButton";
 import { store } from '@/store/store';
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Navbar>
           <NavbarButton text='Home' navPath='/' img={homeIcon} imgActive={homeIconActive} />
           <NavbarButton text="About" navPath="/about" img={aboutIcon} imgActive={aboutIconActive} />
+          <NavbarButton text="Projects" navPath="/projects" img={projectsIcon} imgActive={projectsIconActive} />
         </Navbar>
         <Component {...pageProps} />
       </Provider>
