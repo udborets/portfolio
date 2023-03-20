6f8d29a6982c1583693f1b4a5257d5e9fda7ac67
Yury Borets <udborets@gmail.com>
2023-03-20 10:02:19 +0300

    rename navbar

--- src/components/LeftNavbar.tsx
+++ src/components/LeftNavbar.tsx
@@ -3,11 +3,13 @@ interface NavbarProps {
   className?: string;
 }
 
-const Navbar = ({ children, className }: NavbarProps) => {
+const LeftNavbar = ({ children, className }: NavbarProps) => {
   return (
-    <aside className="navbar nav flex w-[150px] h-full min-w-[150px] 
+    <aside className="navbar hidden nav xl:flex 2xl:flex w-[150px] h-full min-w-[150px] 
         items-start flex-col justify-center 
-      bg-black px-2 z-[2]">
+      bg-black px-2 z-[2] 
+        xl:visible
+      ">
       <nav>
         <ul className={"nav__menu flex flex-col aling-center h-fit justify-around gap-8 " + className ?? ""}>
           {children}
@@ -17,4 +19,4 @@ const Navbar = ({ children, className }: NavbarProps) => {
   )
 }
 
-export default Navbar
+export default LeftNavbar
