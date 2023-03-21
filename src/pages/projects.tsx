147731e74ba1593968fb637c02d623a270ddd1ac
Yury Borets <udborets@gmail.com>
2023-03-21 22:26:28 +0300

    center projects in projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -29,7 +29,7 @@ const Projects = () => {
         <title>udborets/projects</title>
       </Head>
       <Page movingFromTop>
-        <div className="projects grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 2xl:grid-cols-4 gap-[10px] flex-wrap">
+        <div className="projects grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 2xl:grid-cols-4 gap-[10px] flex-wrap content-center w-full h-full">
           {repos
             ? repos.map((repo, i) => (
               <ProjectItem key={repo.id} repo={repo} transitionDelayIndex={i + 1} />
