7014f53c399474be3a975716985cd2563ab31449
Yury Borets <udborets@gmail.com>
2023-03-21 21:48:51 +0300

    fix dev icons stylesheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="https://img.icons8.com/external-others-inmotus-design/67/null/external-U-qwerty-keypad-others-inmotus-design-3.png" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
       </Head>
       <body>
         <Main />
