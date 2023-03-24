5e7536628dd31969d6b6c985ea33d6dfbb83d63b
Yury Borets <udborets@gmail.com>
2023-03-24 08:07:42 +0300

    delete page reducer from redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { navbarReducer } from "./slices/navbarSlice";
-import { pageReducer } from "./slices/pageSlice";
 
 export const store = configureStore({
   reducer: {
     navbar: navbarReducer,
-    page: pageReducer,
   },
 });
-const storeState = store.getState()
-export type Store = typeof storeState
+const storeState = store.getState();
+export type Store = typeof storeState;
