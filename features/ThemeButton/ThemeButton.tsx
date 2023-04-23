e4b77fd8f3744b0b82b5d62dba86b7527233a17b
Yury Borets <udborets@gmail.com>
2023-04-23 15:03:14 +0300

    fix theme button styles

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const ThemeButton = () => {
   return (
     <input
-      className={`ThemeButton ${styles.ThemeButton} bg-black theme-transition`}
+      className={`${styles.ThemeButton} absolute right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]`}
       id="theme-button"
       type="checkbox"
     />
