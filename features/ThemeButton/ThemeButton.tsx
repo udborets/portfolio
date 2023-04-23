5ee3f0fdc74b62cc539ad6f347395a06234105ab
Yury Borets <udborets@gmail.com>
2023-04-23 17:10:02 +0300

    fix theme button styles

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -1,13 +1,11 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { ThemeColors } from "./models";
 
-import styles from "./styles.module.scss";
-
 const ThemeButton = () => {
   const [theme, setTheme] = useLocalStorage({ key: 'theme', initValue: ThemeColors.DARK });
   return (
     <input
-      className={`${styles.ThemeButton} absolute hover:cursor-pointer right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]`}
+      className="ThemeButton bg-inverse absolute appearance-none hover:cursor-pointer right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]"
       id="theme-button"
       type="checkbox"
       checked={theme === ThemeColors.LIGHT}
