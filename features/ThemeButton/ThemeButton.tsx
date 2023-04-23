665275f5374b833e0c1b12abc1947693ce7ce31d
Yury Borets <udborets@gmail.com>
2023-04-23 16:00:00 +0300

    add useLocalStorage hook usage in theme button

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -1,11 +1,18 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { ThemeColors } from "./models";
+
 import styles from "./styles.module.scss";
 
 const ThemeButton = () => {
+  const [theme, setTheme] = useLocalStorage({ key: 'theme', initValue: ThemeColors.DARK });
   return (
     <input
       className={`${styles.ThemeButton} absolute hover:cursor-pointer right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]`}
       id="theme-button"
       type="checkbox"
+      checked={theme === ThemeColors.LIGHT}
+      onClick={() => setTheme((oldThemeState: string) => oldThemeState === ThemeColors.DARK ? ThemeColors.LIGHT : ThemeColors.DARK)}
+      readOnly
     />
   )
 }
