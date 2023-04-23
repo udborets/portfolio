1e0ef1dc22c3267c892253d0d410dbcc53db7f12
Yury Borets <udborets@gmail.com>
2023-04-23 16:53:59 +0300

    add main page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { outfit } from "@/fonts/index";
+
 const HomePage = () => {
   return (
     <>
@@ -8,7 +10,10 @@ const HomePage = () => {
           Yury Borets
         </title>
       </Head>
-      <main className="HomePage w-full h-full bg-main theme-transition">
+      <main className="HomePage w-full h-full flex flex-col justify-center items-center">
+        <h1 className={`Title font-bold text-main inline text-[5rem] ${outfit.className}`}>
+          Yury Borets
+        </h1>
       </main>
     </>
   )
