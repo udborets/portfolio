c74eb9ce4e7b3f4e1889777e8ea886c463b7545d
Yury Borets <udborets@gmail.com>
2023-04-23 18:48:20 +0300

    add greeting component to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-import { outfit } from "@/fonts/index";
+import Greeting from "@/features/Greeting/Greeting";
 
 const HomePage = () => {
   return (
@@ -10,13 +10,8 @@ const HomePage = () => {
           Yury Borets
         </title>
       </Head>
-      <main className="HomePage w-full h-full flex flex-col justify-center items-center">
-        <h1 className={`HomePage__title font-bold text-main text-[3rem] sm:text-[5rem] w-fit ${outfit.className}`}>
-          Yury Borets
-        </h1>
-        <h2 className={`HomePage__subTitle font-bold text-main inline text-[2rem] sm:text-[3.5rem] mt-8 ${outfit.className}`}>
-          Frontend Developer
-        </h2>
+      <main className="HomePage h-fit flex flex-col flex-grow justify-center items-center">
+        <Greeting />
       </main>
     </>
   )
