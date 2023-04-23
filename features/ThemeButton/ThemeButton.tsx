343e4436cab5dae27df9d0a906565e86c88e4d25
Yury Borets <udborets@gmail.com>
2023-04-23 15:05:54 +0300

    add cursor-pointer on hover to theme button

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const ThemeButton = () => {
   return (
     <input
-      className={`${styles.ThemeButton} absolute right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]`}
+      className={`${styles.ThemeButton} absolute hover:cursor-pointer right-[5px] top-[5px] theme-transition rounded-[10px] w-[20px] h-[20px]`}
       id="theme-button"
       type="checkbox"
     />
