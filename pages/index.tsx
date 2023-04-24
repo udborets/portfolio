575714ee188d1cfc8c930e9fc4af1f0638f71e50
Yury Borets <udborets@gmail.com>
2023-04-24 08:54:50 +0300

    fix main page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
-import Greeting from "@/features/Greeting/Greeting";
+import SectionContainer from "@/features/SectionContainer/SectionContainer";
+import { outfit } from "@/fonts";
 
 const HomePage = () => {
   return (
@@ -11,7 +12,14 @@ const HomePage = () => {
         </title>
       </Head>
       <main className="HomePage h-fit flex flex-col flex-grow justify-center items-center">
-        <Greeting />
+        <SectionContainer className="Main flex flex-col justify-center items-center">
+          <h1 className={`Main__title font-bold text-main text-center text-[2.8rem] sm:text-[5rem] w-fit ${outfit.className}`}>
+            Yury Borets
+          </h1>
+          <h2 className={`Main__subtitle font-bold text-main inline text-center text-[1.8rem] w-fit sm:text-[3.5rem] ${outfit.className}`}>
+            Frontend Developer
+          </h2>
+        </SectionContainer>
       </main>
     </>
   )
