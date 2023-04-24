e76fa5ac55decb9e919d7b1ac58e3d8a8606a015
Yury Borets <udborets@gmail.com>
2023-04-24 13:58:08 +0300

    move section container component to another folder

---
+++ features/HomePageSections/SectionContainer/SectionContainer.tsx
@@ -0,0 +1,12 @@
+import { outfit } from "@/fonts";
+import { ISectionContainerProps } from "./models";
+
+const SectionContainer = ({ children, className }: ISectionContainerProps) => {
+  return (
+    <section className={`${className ?? ""} SectionContainer text-main h-screen min-h-screen w-full p-2 flex ${outfit.className}`}>
+      {children}
+    </section>
+  )
+}
+
+export default SectionContainer
