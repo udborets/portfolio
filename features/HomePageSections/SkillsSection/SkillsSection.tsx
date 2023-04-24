27889902b022c737752bf80477aee903de8cd93d
Yury Borets <udborets@gmail.com>
2023-04-24 19:46:44 +0300

    fix skills section component

--- features/HomePageSections/SkillsSection/SkillsSection.tsx
+++ features/HomePageSections/SkillsSection/SkillsSection.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+
 import SectionContainer from "../SectionContainer/SectionContainer";
+import FieldSelect from "./FieldSelect/FieldSelect";
 import SkillsList from "./SkillsList/SkillsList";
 import { ISkillsSectionProps } from "./models";
 
@@ -7,23 +9,16 @@ const SkillsSection = ({ skills }: ISkillsSectionProps) => {
   const [chosenField, setChosenField] = useState<string>(() => skills[0].fieldName);
   return (
     <SectionContainer className="SkillsSection flex flex-col justify-center items-center gap-10">
-      <h2 className={`SkillsSection__title font-bold text-main inline text-center text-[1.8rem] w-fit sm:text-[3.5rem] }`}>
+      <h2 className={`SkillsSection__title font-bold mt-20 sm:mt-40 text-main inline text-center text-[1.8rem] w-fit sm:text-[3.5rem] }`}>
         My skills
       </h2>
-      <ul className="SkillsSection__list flex flex-col gap-2 text-[1.7rem]">
-        {skills.map((skill) => (
-          <li key={skill.fieldName}>
-            <button
-              onClick={() => setChosenField(skill.fieldName)}
-            >
-              {skill.fieldName}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <SkillsList
-        {...skills.filter(({ fieldName }) => fieldName === chosenField)[0]}
-      />
+      <div className="SkillsSection__body flex w-4/5 h-full flex-col sm:flex-row items-center justify-between gap-8">
+        <FieldSelect
+          selectFieldFunction={setChosenField}
+          fields={skills.map(({ fieldName }) => fieldName)}
+        />
+        <SkillsList {...skills.filter(({ fieldName }) => fieldName === chosenField)[0]} />
+      </div>
     </SectionContainer>
   )
 }
