100254c6ef2f3fb2277271df053626f7eff0e7fe
Yury Borets <udborets@gmail.com>
2023-04-24 21:04:47 +0300

    fix skills list styles

--- features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
+++ features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
@@ -2,11 +2,11 @@ import { ISkillsListProps } from "./models";
 
 const SkillsList = ({ fieldName, list }: ISkillsListProps) => {
   return (
-    <div className="SkillsList flex flex-col w-full sm:w-1/2 items-start justify-center">
-      <h4 className="SkillsList__name font-[600] text-[2rem]">
+    <div className="SkillsList flex flex-col w-full min-h-[300px] h-fit sm:w-1/2 items-start justify-start">
+      <h4 className="SkillsList__name font-[600] text-[1.5rem] sm:text-[3rem]">
         {fieldName}
       </h4>
-      <p className=" text-[1.4rem]">
+      <p className="SkillsList__list h-full text-[1.3rem] sm:text-[2rem]">
         {list.join(', ')}
       </p>
     </div>
