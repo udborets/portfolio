58881c35392812721e2554a35cd2f04b079b8a13
Yury Borets <udborets@gmail.com>
2023-04-25 23:16:58 +0300

    fix theme button styles

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -12,11 +12,11 @@ const ThemeButton = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       onClick={() => setTheme((oldThemeState: string) => oldThemeState === ThemeColors.DARK ? ThemeColors.LIGHT : ThemeColors.DARK)}
-      className="ThemeButton bg-inverse flex justify-center items-center fixed w-[30px] h-[30px] right-[30px] top-[30px] rounded-[50%] hover:rounded-[10px] duration-300 transition-all sm:w-[40px] sm:h-[40px]"
+      className="ThemeButton bg-inverse hover:cursor-pointer flex justify-center items-center fixed w-[30px] h-[30px] right-[30px] top-[30px] rounded-[50%] hover:rounded-[10px] duration-300 transition-all sm:w-[40px] sm:h-[40px]"
     >
       <motion.input
-        className="ThemeButton__input w-3/5 h-3/5 bg-main appearance-none rounded-[50%] hover:rounded-[20px] "
-        animate={{ scale: isHovered ? 1 : 0.5 }}
+        className="ThemeButton__input w-4/5 h-4/5 bg-main appearance-none"
+        animate={{ scale: isHovered ? 1 : 0.5, borderRadius: isHovered ? "50%" : "10px" }}
         transition={{ duration: 0.5, ease: "easeOut" }}
         id="theme-button"
         type="checkbox"
