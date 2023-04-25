95203def2bb6ab733aead8376cbe98e9d28234e5
Yury Borets <udborets@gmail.com>
2023-04-25 23:16:10 +0300

    add scroll progress bar component

---
+++ features/ScrollProgressBar/ScrollProgressBar.tsx
@@ -0,0 +1,14 @@
+import { motion, useScroll } from "framer-motion";
+
+const ScrollProgressBar = () => {
+  const { scrollYProgress } = useScroll();
+  console.log(scrollYProgress)
+  return (
+    <motion.div
+      style={{ scaleX: scrollYProgress }}
+      className='w-full fixed top-0 left-0 bg-inverse h-[3px] sm:h-[4px]'
+    />
+  )
+}
+
+export default ScrollProgressBar
