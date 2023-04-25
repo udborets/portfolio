956d98e26eb06effd3ad0581088b9ca6d8fe4bf1
Yury Borets <udborets@gmail.com>
2023-04-25 21:37:32 +0300

    fix skills list animation

--- features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
+++ features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
@@ -1,15 +1,24 @@
+import { motion } from "framer-motion";
+
 import { ISkillsListProps } from "./models";
 
 const SkillsList = ({ fieldName, list }: ISkillsListProps) => {
   return (
-    <div className="SkillsList flex flex-col w-full min-h-[300px] h-fit sm:min-h-[400px] sm:w-1/2 items-start justify-start">
+    <motion.div
+      className="SkillsList flex flex-col w-full min-h-[300px] h-fit sm:min-h-[400px] sm:w-1/2 items-start justify-start"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      key={fieldName}
+    >
       <h4 className="SkillsList__name font-[600] text-[1.5rem] sm:text-[3rem]">
         {fieldName}
       </h4>
       <p className="SkillsList__list h-full text-[1.3rem] sm:text-[2rem]">
         {list.join(', ')}
       </p>
-    </div>
+    </motion.div>
   )
 }
 
