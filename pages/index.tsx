859445db64821d00ae2fb39d73583b7ec0e1a330
Yury Borets <udborets@gmail.com>
2023-04-25 23:16:30 +0300

    fix home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,30 @@ import MainSection from "@/features/HomePageSections/MainSection/MainSection";
 import SkillsSection from "@/features/HomePageSections/SkillsSection/SkillsSection";
 import { ISkillsSectionProps } from "@/features/HomePageSections/SkillsSection/models";
 
-import styles from "./styles.module.scss";
+const skillsSectionProps: ISkillsSectionProps = {
+  skills:
+    [
+      {
+        list: ["React", "NextJS", "TypeScript", "Redux Toolkit", "SCSS", "Styled Components",
+          "TailwindCSS", "tRPC", "Zustand", "React Router DOM", "Astro", "React Query", "Axios", "React Hook Form", "Framer Motion"],
+        fieldName: "Frontend",
+      },
+      {
+        list: ["NodeJS", "ExpressJS", "tRPC"],
+        fieldName: "Backend",
+      },
+      {
+        list: ['PostgreSQL', "MongoDB", "Prisma ORM"],
+        fieldName: "Databases",
+      },
+      {
+        list: ['Python', "Go"],
+        fieldName: "Other",
+      },
+    ]
+}
 
 const HomePage = () => {
-  const skillsSectionProps: ISkillsSectionProps = {
-    skills:
-      [
-        {
-          list: ["React", "NextJS", "TypeScript", "Redux Toolkit", "SCSS", "Styled Components",
-            "TailwindCSS", "tRPC", "Zustand", "React Router DOM", "Astro", "React Query", "Axios", "React Hook Form", "Framer Motion"],
-          fieldName: "Frontend",
-        },
-        {
-          list: ["NodeJS", "ExpressJS", "tRPC"],
-          fieldName: "Backend",
-        },
-        {
-          list: ['PostgreSQL', "MongoDB", "Prisma ORM"],
-          fieldName: "Databases",
-        },
-        {
-          list: ['Python', "Go"],
-          fieldName: "Other",
-        },
-      ]
-  }
   return (
     <>
       <Head>
@@ -36,7 +35,7 @@ const HomePage = () => {
           Yury Borets
         </title>
       </Head>
-      <main className={`${styles.HomePage} h-screen min-h-screen flex flex-col flex-grow justify-start items-center snap-y snap-mandatory overflow-y-scroll`}>
+      <main className={`HomePage flex flex-col flex-grow justify-start items-center`}>
         <MainSection />
         <SkillsSection {...skillsSectionProps} />
       </main>
