7214771b95cc41e5fae32e39d81d3ec221ed0857
Yury Borets <udborets@gmail.com>
2023-04-25 07:48:25 +0300

    fix field select component height styles

--- features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
+++ features/HomePageSections/SkillsSection/SkillsList/SkillsList.tsx
@@ -2,7 +2,7 @@ import { ISkillsListProps } from "./models";
 
 const SkillsList = ({ fieldName, list }: ISkillsListProps) => {
   return (
-    <div className="SkillsList flex flex-col w-full min-h-[300px] h-fit sm:w-1/2 items-start justify-start">
+    <div className="SkillsList flex flex-col w-full min-h-[300px] h-fit sm:min-h-[400px] sm:w-1/2 items-start justify-start">
       <h4 className="SkillsList__name font-[600] text-[1.5rem] sm:text-[3rem]">
         {fieldName}
       </h4>
