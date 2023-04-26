522d278a61ad77061987cf0f8a27ad9d4a4d1378
Yury Borets <udborets@gmail.com>
2023-04-26 08:34:18 +0300

    add field select item usage in field select

--- features/HomePageSections/SkillsSection/FieldSelect/FieldSelect.tsx
+++ features/HomePageSections/SkillsSection/FieldSelect/FieldSelect.tsx
@@ -1,35 +1,20 @@
-import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 
+import FieldSelectItem from "./FieldSelectItem/FieldSelectItem";
 import { IFieldSelectProps } from "./models";
 
 const FieldSelect = ({ fields, selectFieldFunction, selectedField }: IFieldSelectProps) => {
   const fieldsListRef = useRef<HTMLUListElement>(null);
-  const isInView = useInView(fieldsListRef, { once: true });
   return (
     <ul ref={fieldsListRef} className="FieldSelect flex flex-row flex-wrap sm:flex-col font-[600] justify-center gap-2 sm:gap-8 text-[1.3rem] sm:text-[2rem]">
       {fields.map((fieldName, index) => (
-        <motion.li
+        <FieldSelectItem
+          fieldName={fieldName}
+          index={index}
+          selectFieldFunction={selectFieldFunction}
+          selectedField={selectedField}
           key={fieldName}
-          initial={{
-            translateX: "-100%",
-            opacity: 0,
-          }}
-          animate={{
-            translateX: isInView ? "0" : "-100%",
-            opacity: isInView ? 1 : 0,
-          }}
-          transition={{ ease: "easeOut", duration: 1, delay: index * 0.3 }}
-        >
-          <button
-            className={`FieldSelect__button duration-500 sm:py-3 sm:px-4 ${selectedField === fieldName
-              ? "opacity-100"
-              : "opacity-40 hover:opacity-70"}`}
-            onClick={() => selectFieldFunction(fieldName)}
-          >
-            {fieldName}
-          </button>
-        </motion.li>
+        />
       ))}
     </ul>
   )
