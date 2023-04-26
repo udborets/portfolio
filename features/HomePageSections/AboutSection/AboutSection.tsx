25d048172b6cc6abde8d9d907dcf123b0fd98f4d
Yury Borets <udborets@gmail.com>
2023-04-26 10:24:00 +0300

    add about section component

---
+++ features/HomePageSections/AboutSection/AboutSection.tsx
@@ -0,0 +1,11 @@
+import SectionContainer from "@/features/SectionContainer/SectionContainer"
+
+const AboutSection = () => {
+  return (
+    <SectionContainer className="AboutSection">
+      
+    </SectionContainer>
+  )
+}
+
+export default AboutSection
