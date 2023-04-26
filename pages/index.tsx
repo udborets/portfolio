30f5be1bb25d51fd42ac35c37469018fab1ce87e
Yury Borets <udborets@gmail.com>
2023-04-26 11:03:05 +0300

    fix home page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const HomePage = () => {
           Yury Borets
         </title>
       </Head>
-      <main ref={scrollContainerRef} className={`HomePage flex flex-col flex-grow justify-start overflow-y-scroll snap-y snap-mandatory scroll-smooth h-screen min-h-screen items-center`}>
+      <main ref={scrollContainerRef} className={`HomePage flex flex-col flex-grow scrollContainer justify-start overflow-y-scroll snap-y snap-mandatory scroll-smooth h-screen min-h-screen items-center`}>
         <MainSection />
         <AboutSection />
         <SkillsSection {...skillsSectionProps} />
