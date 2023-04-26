bddc7829c2320eacbaeeb2487670cdba256aa38d
Yury Borets <udborets@gmail.com>
2023-04-26 11:02:53 +0300

    remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import ScrollProgressBar from '@/features/ScrollProgressBar/ScrollProgressBar';
 import ThemeButton from '@/features/ThemeButton/ThemeButton';
 
 import '@/styles/global.scss';
