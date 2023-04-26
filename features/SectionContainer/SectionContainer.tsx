568dc0d8e9c4f55ef1a7920bf69d361f5bc93877
Yury Borets <udborets@gmail.com>
2023-04-26 10:55:48 +0300

    fix section container snap styles

--- features/SectionContainer/SectionContainer.tsx
+++ features/SectionContainer/SectionContainer.tsx
@@ -3,7 +3,7 @@ import { ISectionContainerProps } from "./models";
 
 const SectionContainer = ({ children, className }: ISectionContainerProps) => {
   return (
-    <section className={`${className ?? ""} SectionContainer text-main h-screen min-h-screen w-full p-2 flex ${outfit.className}`}>
+    <section className={`${className ?? ""} SectionContainer snap-start text-main h-screen min-h-screen w-full p-2 flex ${outfit.className}`}>
       {children}
     </section>
   )
