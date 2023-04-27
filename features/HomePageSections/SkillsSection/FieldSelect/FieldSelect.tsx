ee44370ad514d4b70770d5c393dc4da75fdba37a
Yury Borets <udborets@gmail.com>
2023-04-27 18:49:45 +0300

    fix field select styles

--- features/HomePageSections/SkillsSection/FieldSelect/FieldSelect.tsx
+++ features/HomePageSections/SkillsSection/FieldSelect/FieldSelect.tsx
@@ -2,11 +2,16 @@ import { useRef } from "react";
 
 import FieldSelectItem from "./FieldSelectItem/FieldSelectItem";
 import { IFieldSelectProps } from "./models";
+import styles from './styles.module.scss';
 
 const FieldSelect = ({ fields, selectFieldFunction, selectedField }: IFieldSelectProps) => {
   const fieldsListRef = useRef<HTMLUListElement>(null);
   return (
-    <ul ref={fieldsListRef} className="FieldSelect flex flex-row flex-wrap sm:flex-col font-[600] justify-center gap-2 sm:gap-8 text-[1.3rem] sm:text-[2rem]">
+    <ul
+      ref={fieldsListRef}
+      className={`${styles.FieldSelect} flex flex-row flex-wrap sm:flex-col font-[600] justify-center 
+      gap-2 sm:gap-8 text-[1.3rem] sm:text-[2rem]`}
+    >
       {fields.map((fieldName, index) => (
         <FieldSelectItem
           fieldName={fieldName}
