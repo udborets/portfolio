2a5b9e3a38102484338ab7ad6337cc2a7a41a22e
Yury Borets <udborets@gmail.com>
2023-04-29 05:47:29 +0300

    change theme button input centering way

--- features/ThemeButton/ThemeButton.tsx
+++ features/ThemeButton/ThemeButton.tsx
@@ -11,10 +11,10 @@ const ThemeButton = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       onClick={() => setTheme((oldThemeState: string) => oldThemeState === ThemeColors.DARK ? ThemeColors.LIGHT : ThemeColors.DARK)}
-      className="ThemeButton bg-inverse hover:cursor-pointer flex justify-center items-center fixed w-[30px] h-[30px] right-[30px] top-[30px] rounded-[50%] hover:rounded-[10px] duration-300 transition-all sm:w-[40px] sm:h-[40px]"
+      className="ThemeButton bg-inverse hover:cursor-pointer fixed w-[30px] h-[30px] right-[30px] top-[30px] rounded-[50%] hover:rounded-[10px] duration-300 transition-all sm:w-[40px] sm:h-[40px]"
     >
       <input
-        className={`ThemeButton__input bg-main appearance-none hover:cursor-pointer rounded w-3/5 h-3/5 transition-all duration-300 ${isHovered
+        className={`ThemeButton__input bg-main appearance-none absolute inset-0 m-auto hover:cursor-pointer rounded w-3/5 h-3/5 transition-all duration-300 ${isHovered
           ? 'rounded-[50%]'
           : 'rounded-[5px]'}`}
         id="theme-button"
