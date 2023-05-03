ea49048278a753672ce5acc20e5ddfee58b631dd
Yury Borets <udborets@gmail.com>
2023-05-03 16:06:20 +0300

    add some home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import { FC } from "react";
 
 import PageLayout from "@/layouts/PageLayout/PageLayout";
+import styles from './home.module.scss';
+import { robotoMono } from "@/fonts";
 
 const HomePage: FC = () => {
   return (
     <PageLayout title="Yury Borets">
-      <main className="home-page">
+      <main className={`${styles["home-page"]} ${robotoMono.className}`}>
+        <div className={`${styles["greeting"]}`}>
+          <span className={`${styles["pre-name"]}`}>
+            Hi, my name is
+          </span>
+          <h1 className={`${styles["name"]}`}>
+            Yury Borets
+          </h1>
+          <span className={`${styles["post-name"]}`}>
+            and I am a web developer
+          </span>
+        </div>
       </main>
     </PageLayout>
   )
