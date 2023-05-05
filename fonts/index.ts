171c1fdb053c0606141703909aec83d31d1e4bb8
Yury Borets <udborets@gmail.com>
2023-05-05 11:56:48 +0300

    delete old files

--- fonts/index.ts
+++
@@ -1,6 +0,0 @@
-import { Roboto_Mono } from "next/font/google";
-
-export const robotoMono = Roboto_Mono({
-  subsets: ["cyrillic", "latin"],
-  weight: ["400", "700"],
-});
--- layouts/PageLayout/models.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-
-export type IPageLayoutProps = {
-  title?: string;
-  children: ReactNode;
-};
